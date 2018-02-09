6b275b90360a90f644cf823cf923c5490011143b
Fabien Piuzzi <reefab@demenzia.net>
2018-02-09 14:14:42 +0100

    Incremented version for first Pypi upload

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name="foobot_async",
-    version="0.0.1",
+    version="0.1.0",
     url="https://github.com/reefab/foobot_async",
 
     author="Fabien Piuzzi",
     author_email="fabien@reefab.net",
 
-    long_description='asyncio-friendly python API for Foobot Air Quality Monitors (https://foobot.io). Requires Python 3.4+',
+    long_description='asyncio-friendly python API for Foobot Air Quality Monitors (https://foobot.io). Requires Python 3.5+',
     license='MIT',
 
     packages=setuptools.find_packages(),
@@ -17,7 +17,7 @@ setuptools.setup(
     zip_safe=True,
 
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
